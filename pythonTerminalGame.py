bc1dea64d238e78c987a747f4c236688a244cd80
Aaron Loera <loeraaaron776@gmail.com>
2023-06-10 15:54:41 -0500

    Adding count pieces method to the Player class

--- pythonTerminalGame.py
+++ pythonTerminalGame.py
@@ -39,58 +39,83 @@ class Player:
     
     #returns the new Board after a Player has made a move
     def make_move(self, board_name, list_index, sublist_index):
-       board_name.board_as_list[list_index+1][1][sublist_index] = self.color[0]
+       board_name.board_as_list[list_index][1][sublist_index-1] = self.color[0]
        return board_name
     
-board1 = Board()
-player1 = Player("Aaron", "Red")
-#print(board1.board_as_list)
-print(board1)
-print(player1.make_move(board1, 5, 0))
+    def total_pieces(self, board_name):
+        count = 0
+        for row in board_name.board_as_list:
+            for item in row[1]:
+                if self.color[0] == item:
+                    count +=1
+        return count
+    
+# board1 = Board()
+# player1 = Player("A", "Red")
 
+#asks for player1 name data with user input
+player1_name = input("Welcome to Connect Four! Hello Player 1, can I have your name please: ")
 
-# #asks for player1 name data with user input
-# player1_name = input("Welcome to Connect Four! Hello Player 1, can I have your name please: ")
+#creates player1 with name
+player1 = Player(player1_name)
 
-# #creates player1 with name
-# player1 = Player(player1_name)
+#asks for player2 data with user input
+player2_name = input("Welcome Player 2, could I also have your name please: ")
 
-# #asks for player2 data with user input
-# player2_name = input("Welcome Player 2, could I also have your name please: ")
+#creates player2
+player2 = Player(player2_name)
 
-# #creates player2
-# player2 = Player(player2_name)
+#determines the color choice for player1
+color_choice_player1 = input("Welcome " + player1.name + " and " + player2.name + 
+                     ". In connect four the objective is to connect four dots before the other player." +
+                     " You can connect your pieces either horiontally, vertially, or diagonally." +
+                     " Now that you know the basics, it's time to start! Player 1 would you like 'Red' or 'Blue': ")
 
-# #determines the color choice for player1
-# color_choice_player1 = input("Welcome " + player1.name + " and " + player2.name + 
-#                      ". In connect four the objective is to connect four dots before the other player." +
-#                      " You can connect your pieces either horiontally, vertially, or diagonally." +
-#                      " Now that you know the basics, it's time to start! Player 1 would you like 'Red' or 'Blue': ")
+#checks if user input is invalid for color choice of player1
+while color_choice_player1 != "Red" and color_choice_player1 != "Blue":
+    color_choice_player1 = input("Sorry I didn't get that, could you please choose either 'Red' or 'Blue': ")
 
-# #checks if user input is invalid for color choice of player1
-# while color_choice_player1 != "Red" and color_choice_player1 != "Blue":
-#     color_choice_player1 = input("Sorry I didn't get that, could you please choose either 'Red' or 'Blue': ")
+#assigns the color chosesn to player1 object
+player1.color = color_choice_player1
 
-# #assigns the color chosesn to player1 object
-# player1.color = color_choice_player1
+#assigns other color to player2
+if color_choice_player1 == "Red":
+    color_choice_player2 = "Blue"
+else:
+    color_choice_player2 = "Red"
 
-# #assigns other color to player2
-# if color_choice_player1 == "Red":
-#     color_choice_player2 = "Blue"
-# else:
-#     color_choice_player2 = "Red"
+#assigns the color to player2 object
+player2.color = color_choice_player2
 
-# #assigns the color to player2 object
-# player2.color = color_choice_player2
+#instatiates the connect4 board
+connect4_board = Board()
 
-# #instatiates the connect4 board
-# connect4_board = Board()
+#starts the game
+player1_move = input("Alright, that means you're " + player2.color + " " + player2.name + 
+                   ". Now that we have everything set it's time to begin the game. This is the board you will be playing on.\n\n" +
+                   str(connect4_board) + "\nAlright " + player1.name 
+                   + ", pick where you would like place your piece with the letter firt and then the number: ")
 
-# #starts the game
-# player1_move = input("Alright, that means you're " + player2.color + " " + player2.name + 
-#                    ". Now that we have everything set it's time to begin the game. This is the board you will be playing on.\n\n" +
-#                    str(connect4_board) + "\nAlright " + player1.name 
-#                    + ", pick which row you would like to place your piece: ")
+#turns player1's input into a list
+player1_placement = [num for num in player1_move]
 
-# #player1 makes the first move
-# print(player1_move)
+#translates player1's input into the coordinate on the board
+for row in connect4_board.board_as_list:
+    if player1_placement[0] == row[0]:
+        list = connect4_board.board_as_list.index(row)
+        sublist = int(player1_placement[1])
+        print(player1.make_move(connect4_board, list, sublist))
+
+#asks for player2's move
+player2_move = input("Great, now it's your turn " + player2.name + ". Pick where your would like to place your piece: ")
+
+#turns player1's input into a list
+player2_placement = [num for num in player2_move]
+
+#translates player2's input into the coordinate on the board
+for row in connect4_board.board_as_list:
+    if player2_placement[0] == row[0]:
+        list = connect4_board.board_as_list.index(row)
+        sublist = int(player2_placement[1])
+        print(player2.make_move(connect4_board, list, sublist))
+
